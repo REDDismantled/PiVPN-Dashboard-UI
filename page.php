65107d5cfecad99d1b70d3cacbad51fd5d8835e3
REDD <66808379+REDDismantled@users.noreply.github.com>
2020-07-14 16:35:15 -0700

    Update page.php

--- page.php
+++ page.php
@@ -1,4 +1,17 @@
 <?php
+//Basic Dbug
+//ini_set('display_errors', 1);
+//ini_set('display_startup_errors', 1);
+//error_reporting(E_ALL);
+
+ini_set('error_reporting', E_ALL & ~E_NOTICE & ~E_STRICT & ~E_DEPRECATED);
+ini_set('max_execution_time',300);
+
+define("OPENRSD", true);
+define("BASEURI", dirname($_SERVER['SCRIPT_NAME'])."/");
+
+require_once('app/functions.php');
+
 session_start();
 if(!isset($_SESSION['username'])){
 	die("You must be logged in to view this page!");
@@ -23,14 +36,14 @@ if(isset($_POST['page'])){
 
 //Simple page functions..
 function openvpn(){
-	include('pages/openvpn.php');
+	include('app/openvpn.php');
 }
 function blocked()
 {
-	include('pages/block.php');
+	include('app/block.php');
 }
 function logs()
 {
-	include('pages/logs.php');
+	include('app/logs.php');
 }
 ?>
