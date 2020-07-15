5c1a35683e96503b23491ad739ecebc26e8c7970
REDD <66808379+REDDismantled@users.noreply.github.com>
2020-07-14 18:27:00 -0700

    Update profile.php

--- app/profile.php
+++ app/profile.php
@@ -11,10 +11,15 @@ if (isset($_POST['days']))
 {
     $days = $_POST['days'];
 }
+$pass = defaultpassword; // Days is set with a default prompt value.
+if (isset($_POST['pass']))
+{
+    $days = $_POST['pass'];
+}
 $pro = $_POST['profile'];
-add_vpn_profile($pro, $days);
+add_vpn_profile($pro, $days, $pass);
 //Run selected script, but only if it exists in the scr_up folder.
-function add_vpn_profile($profile, $d) {
+function add_vpn_profile($profile, $d, $s) {
 	
     // Open a handle to expect in write mode
     $p = popen('sudo /usr/bin/expect','w');
@@ -26,11 +31,15 @@ function add_vpn_profile($profile, $d) {
     // Spawn a shell as $user
     $cmd .= "spawn /bin/bash; ";
     // Change the unix password
-    $cmd .= "send \"pivpn add nopass\\r\"; ";
+    $cmd .= "send \"pivpn add\\r\"; ";
     $cmd .= "expect \"Enter a Name for the Client:   \"; ";
     $cmd .= "send \"$profile\\r\"; ";
     $cmd .= "expect \"How many days should the certificate last?  $d\"; ";
     $cmd .= "send \"\\b\\b\\b\\b$d\\r\"; ";
+    $cmd .= "expect \"Enter the password for the client:   \"; ";
+    $cmd .= "send \"$pass\\r\"; ";
+    $cmd .= "expect \"Enter the password again to verify:   \"; ";
+    $cmd .= "send \"$pass\\r\"; ";
     $cmd .= "expect \"for easy transfer.\"; ";
     // Commit the command to expect & close
     fwrite($p, $cmd); pclose ($p);
