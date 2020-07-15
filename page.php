6e8f0df03505f48961aad49f5042611dfd789de4
REDD <66808379+REDDismantled@users.noreply.github.com>
2020-07-14 17:15:43 -0700

    Update page.php

--- page.php
+++ page.php
@@ -4,11 +4,11 @@
 //ini_set('display_startup_errors', 1);
 //error_reporting(E_ALL);
 
-//ini_set('error_reporting', E_ALL & ~E_NOTICE & ~E_STRICT & ~E_DEPRECATED);
+ini_set('error_reporting', E_ALL & ~E_NOTICE & ~E_STRICT & ~E_DEPRECATED);
 ini_set('max_execution_time',300);
 
 define("UI", true);
-//define("BASEURI", dirname($_SERVER['SCRIPT_NAME'])."/");
+define("BASEURI", dirname($_SERVER['SCRIPT_NAME'])."/");
 
 require_once('app/functions.php');
 if (!isset($_SESSION)) {
@@ -18,7 +18,7 @@ if(!isset($_SESSION['username'])){
 	die("You must be logged in to view this page!");
 }
 if (isset($_POST['page'])) {
-		Event::handle('PageLoad',array($_SESSION,&$_POST));
+//		Event::handle('PageLoad',array($_SESSION,&$_POST));
         switch ($_POST['page']) {
         case "logs":
         	logs();
