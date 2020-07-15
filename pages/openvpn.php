91d3c61681678ab352515628da02df0b060d3532
REDD <66808379+REDDismantled@users.noreply.github.com>
2020-07-14 17:04:02 -0700

    Update openvpn.php

--- pages/openvpn.php
+++ pages/openvpn.php
@@ -1,7 +1,12 @@
-<?php
+<?php if (!defined("UI")) {
+    die();
+}
+die();
 //Check for valid session:
-session_start();
-include('app/functions.php');
+if (!isset($_SESSION)) {
+    session_start();
+};
+require_once('app/functions.php');
 if(!isset($_SESSION['username'])){
 	die("You must be logged in to view this page!");
 }
