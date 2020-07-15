fbb1016fd1a3b6cf27f78610df1c5b8406459e5e
REDD <66808379+REDDismantled@users.noreply.github.com>
2020-07-14 18:27:51 -0700

    Update power.php

--- app/power.php
+++ app/power.php
@@ -1,6 +1,8 @@
 <?php
 //Check for valid session:
-session_start();
+if (!isset($_SESSION)) {
+    session_start();
+};
 include('functions.php');
 if(!isset($_SESSION['username'])){
 	die("You must be logged in to view this page!");
