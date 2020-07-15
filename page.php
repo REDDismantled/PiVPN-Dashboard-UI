a6ba3802c1ef59ce768053b127780591cc424394
REDD <66808379+REDDismantled@users.noreply.github.com>
2020-07-14 17:07:29 -0700

    Update page.php

--- page.php
+++ page.php
@@ -4,7 +4,16 @@
 //ini_set('display_startup_errors', 1);
 //error_reporting(E_ALL);
 
-session_start();
+ini_set('error_reporting', E_ALL & ~E_NOTICE & ~E_STRICT & ~E_DEPRECATED);
+ini_set('max_execution_time',300);
+
+define("UI", true);
+define("BASEURI", dirname($_SERVER['SCRIPT_NAME'])."/");
+
+require_once('app/functions.php');
+if (!isset($_SESSION)) {
+    session_start();
+};
 if(!isset($_SESSION['username'])){
 	die("You must be logged in to view this page!");
 }
